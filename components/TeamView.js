dd5a6da2bc9d698b4bd2e5fe03ccb603bdb883a5
André Nogueira <vaaz.pro@gmail.com>
2019-01-24 18:03:54 +0000

    Update Team View

--- components/TeamView.js
+++ components/TeamView.js
@@ -15,7 +15,9 @@ TeamView extends Component {
         super(props)
         this.state={
             employees: this.props.employees,
-            icon:this.props.icon
+            icon:this.props.icon,
+            employeesList : [],
+            image: ""
         }
 
     }
@@ -27,10 +29,9 @@ TeamView extends Component {
         
     }
 
-    
-
-    render() {
+    componentWillMount(){
         var employeesList = []
+        var image = ""
         let offset = -10
         if(this.state.employees.length<=5){
             this.state.employees.forEach((employee, index) => {
@@ -51,60 +52,43 @@ TeamView extends Component {
             var others = this.state.employees.length -5
             employeesList.push(
                 <View  key={"others"} style={{flexDirection: "row", marginTop: 6, marginLeft: 10}}>
-                    <Text> +{others} </Text>,
+                    <Text> +{others} </Text>
                     <IconSearch name='users' biblio='Feather' color='black' size={13} />
                 </View>
             );
         }
         if(this.state.icon.substring(0,1)==='#'){
-            var x = (<View style= {{flex: 2, borderTopLeftRadius: 5 , borderBottomLeftRadius:5, justifyContent:'center', alignItems: 'center', marginBottom: 10, marginTop: 10, borderRightColor: 'rgb(216,217,221)', borderRightWidth:2}}>
+            image = (<View style= {{flex: 2, borderTopLeftRadius: 5 , borderBottomLeftRadius:5, justifyContent:'center', alignItems: 'center', marginBottom: 10, marginTop: 10, borderRightColor: 'rgb(216,217,221)', borderRightWidth:2}}>
                 <View style= {{justifyContent:'center', alignItems: 'center',width:20, height: 20,borderRadius: 10, borderColor: 'black', borderWidth: 1 , backgroundColor:this.state.icon}}></View>
             </View>)
         }else{
-            var x = (<View style= {{flex: 2, borderTopLeftRadius: 5 , borderBottomLeftRadius:5, justifyContent:'center', alignItems: 'center', marginBottom: 10, marginTop: 10, borderRightColor: 'rgb(216,217,221)', borderRightWidth:2}}>
+            image = (<View style= {{flex: 2, borderTopLeftRadius: 5 , borderBottomLeftRadius:5, justifyContent:'center', alignItems: 'center', marginBottom: 10, marginTop: 10, borderRightColor: 'rgb(216,217,221)', borderRightWidth:2}}>
                 {/* <CircularPhoto image= {'http://ims-demoipvc.sparkleit.pt/'+ this.changeIcon()+'.png?format=png&width=100%'} size={25}/> */}
                 <IconSearch name={this.changeIcon()} biblio='' color='black' size={25} />
             </View>)
         }
-        
-        
 
+        this.setState({
+            employeesList: employeesList,
+            image: image
+        })
+    }
+
+    
+
+    render() {
         return (
             <View style={[Styles.shadow, {height: 70, flexDirection: 'row', borderRadius: 5, elevation: 1,marginBottom: 20,backgroundColor: '#F2F2F2'}]}>
-                {/*  <View style= {{flex: 2, borderTopLeftRadius: 5 , borderBottomLeftRadius:5, justifyContent:'center', alignItems: 'center', marginBottom: 10, marginTop: 10, borderRightColor: 'rgb(216,217,221)', borderRightWidth:2}}>
-                    <IconSearch name={this.props.iconName} biblio={this.props.iconBiblio} size={this.props.iconSize} color={this.props.iconColor} ></IconSearch>
-                </View>  */}
-                {x}
+                {this.state.image}
                 <View style= {{flex: 6, flexDirection: 'column', marginLeft:20, justifyContent: 'center'}}>
                     <View style= {{flex: 1, justifyContent: 'flex-end'}}>
                         <Text style={{fontSize:18}}>{this.props.txt}</Text>  
                     </View>
                     <View style= {{flex: 1, flexDirection: 'row', alignItems: 'center'}}>
-                        {/* <View style={{backgroundColor: 'white', width: 27, height: 27 , borderRadius: 13.5, alignItems:'center', justifyContent: 'center'}}>
-                            <CircularPhoto image= {this.props.photo1} size={25}/>
-                        </View>
-                        <View style={{position: 'absolute', marginLeft: 20 ,backgroundColor: 'white', width: 27, height: 27 , borderRadius: 13.5, alignItems:'center', justifyContent: 'center'}}>
-                            <CircularPhoto image= {this.props.photo2} size={25}/>
-                        </View>
-                        <View style={{position: 'absolute', marginLeft: 40,backgroundColor: 'white', width: 27, height: 27 , borderRadius: 13.5, alignItems:'center', justifyContent: 'center'}}>
-                            <CircularPhoto image= {this.props.photo3} size={25}/>
-                        </View> */}
-                        {employeesList}
-                        {/* <View style={{position: 'absolute', marginLeft: 60,backgroundColor: 'white', width: 27, height: 27 , borderRadius: 13.5, alignItems:'center', justifyContent: 'center'}}>
-                            <View style={{paddingTop: 10 ,backgroundColor: 'rgb(216,217,221)', width: 25, height: 25 , borderRadius: 12.5, alignItems:'center', justifyContent: 'center'}}>
-                                
-                            <IconSearch name='dots-horizontal' biblio='MaterialCommunityIcons' size={20} color="#000000"></IconSearch>
-                                
-
-                            </View>  
-                        </View> */}
-                        
+                        {this.state.employeesList}
                     </View>
                 </View>    
                 <View style= {{flex: 2, justifyContent: 'center', alignItems: 'center'}}>
-                    {/* <TouchableOpacity>
-                        <IconSearch name='close' biblio='FontAwesome' color='black' size={25} />
-                    </TouchableOpacity> */}
                 </View>                                 
             </View>
         )
